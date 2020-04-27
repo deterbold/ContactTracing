ef0dd395469f43efe57e4700577e580b68f01525
Miguel Sicart <deterbold@gmail.com>
2020-04-27 15:37:57 +0200

    Initial Commit

---
+++ ContactTracing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactTracing
+//
+//  Created by Miguel Angel Sicart on 27/04/2020.
+//  Copyright © 2020 playable_systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
